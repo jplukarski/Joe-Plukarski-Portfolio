2949723ce9caed35fccc520dfc50859e79cf6f91
JPlukarski <jwplukarski@gmail.com>
2018-12-23 13:31:08 -0600

    fixed dessert gif link

--- script.js
+++ script.js
@@ -180,3 +180,16 @@ $(document).ready(function () {
 
 })
 
+
+// Bubble sort solution
+while (!sorted) {
+    sorted = true
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+            sorted = false;
+            var temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
